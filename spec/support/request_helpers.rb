ebee125eb39d52b3470c60e0d0d882ddb13e7a45
MostafaHosny <mostafa.hosny2015@hotmail.com>
2024-05-28 14:05:16 +0200

    configure rspec and add support methods

---
+++ spec/support/request_helpers.rb
@@ -0,0 +1,27 @@
+
+module RequestHelpers
+  def json_headers
+    {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json'
+    }
+  end
+
+  def json
+    return if response.body.blank?
+
+    Oj.load(response.body, symbol_keys: true)
+  end
+
+  def json_post(url, params: {}, headers: {}, auth: false, auth_user: nil)
+    post url, params:, as: :json, headers: prepare_headers(headers, auth, auth_user)
+  end
+
+  def json_get(url, params: {}, headers: {}, auth: false, auth_user: nil)
+    get url, params:, headers: prepare_headers(headers, auth, auth_user)
+  end
+
+  def prepare_headers(spec_headers, _auth, _auth_user)
+    spec_headers.reverse_merge(json_headers)
+  end
+end
