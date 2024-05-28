df41c116c24057a249273105d6c6541383719d98
MostafaHosny <mostafa.hosny2015@hotmail.com>
2024-05-28 14:15:59 +0200

    update user specs to use the support files and gems

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :request do
-  describe 'POST' do
+  describe 'POST Users' do
     context 'with valid parameters' do
-      let(:valid_attributes) { FactoryBot.attributes_for(:user) }
+      let(:valid_attributes) { attributes_for(:user) }
 
       it 'creates a new user' do
         expect do
-          post '/api/v1/users', params: { user: valid_attributes }
+          json_post api_v1_users_path, params: { user: valid_attributes }
         end.to change(User, :count).by(1)
 
         expect(response).to have_http_status(:created)
@@ -15,34 +15,34 @@ RSpec.describe Api::V1::UsersController, type: :request do
     end
 
     context 'with invalid parameters' do
-      let(:invalid_attributes) { FactoryBot.attributes_for(:user, email: 'invalid_email') }
+      let(:invalid_attributes) { attributes_for(:user, email: 'invalid_email') }
 
       it 'does not create a new user' do
         expect do
-          post '/api/v1/users', params: { user: invalid_attributes }
+          json_post api_v1_users_path, params: { user: invalid_attributes }
         end.to_not change(User, :count)
 
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(json['errors']).to include('Email is invalid')
+        expect(json[:errors]).to include('Email is invalid')
       end
     end
   end
 
-  describe 'GET /api/v1/users/:id' do
+  describe 'GET user' do
     let(:user) { create(:user) }
 
     it 'renders a successful response' do
-      get "/api/v1/users/#{user.id}"
+      json_get api_v1_user_path user.id
       expect(response).to be_successful
-      expect(json['id']).to eq(user.id)
-      expect(json['name']).to eq(user.name)
-      expect(json['email']).to eq(user.email)
+      expect(json[:id]).to eq(user.id)
+      expect(json[:name]).to eq(user.name)
+      expect(json[:email]).to eq(user.email)
     end
 
     it 'renders a not found response' do
-      get '/api/v1/users/invalid_id'
+      json_get api_v1_user_path 'invalid_id'
       expect(response).to have_http_status(:not_found)
-      expect(json['error']).to eq('User not found')
+      expect(json[:error]).to eq('User not found')
     end
   end
 end
