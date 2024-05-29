2547909cc6fac7abc3acbb4fa33b0973952ec575
MostafaHosny <mostafa.hosny2015@hotmail.com>
2024-05-29 10:29:49 +0200

    Ensure uniqueness for user and movie combination on the rating tabel

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,7 @@ class Rating < ApplicationRecord
   validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
   validates :user, presence: true
   validates :movie, presence: true
+  validates :user_id, uniqueness: { scope: :movie_id } # Ensure uniqueness for user and movie combination
 
   belongs_to :user
   belongs_to :movie
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_080614) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_082653) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_080614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_ratings_on_movie_id"
+    t.index ["user_id", "movie_id"], name: "index_ratings_on_user_id_and_movie_id", unique: true
     t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
